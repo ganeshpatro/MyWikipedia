70aea069b6110eb9d6a9f329c2e7edef00246e20
Ganesh Patro <ganesh.patro.tech@gmail.com>
2018-08-02 00:32:16 +0530

    Added dummy files to empy groups

---
+++ App/Resources/Resources.swift
@@ -0,0 +1,13 @@
+//
+//  Resources.swift
+//  MyWikipedia
+//
+//  Created by Ganesh Patro on 8/2/18.
+//  Copyright © 2018 Ganesh Patro. All rights reserved.
+//
+
+import UIKit
+
+class Resources: NSObject {
+
+}
